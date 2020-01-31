9f3a8f026370f815c90baf08725407df14275357
Quentin Crida <qcrida@gmail.com>
2020-01-31 15:48:18 +0000

    initial commit

---
+++ src/test/java/PassengerTest.java
@@ -0,0 +1,22 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PassengerTest {
+
+    private Passenger passenger;
+
+    @Before
+
+    public void before(){
+        passenger = new Passenger("Liliana Crida", 3);
+    }
+
+    @Test
+
+    public void canGetPassengerName(){
+        assertEquals("Liliana Crida", passenger.getName());
+    }
+
+}
